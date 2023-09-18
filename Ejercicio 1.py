260f31af08eb3f67700c24ccde1ed1b06edc65cf
edison vilchez <tu@email>
2023-09-17 21:38:23 -0300

    Mi primer ejercicio subido a git

---
+++ Ejercicio 1.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ediso
+#
+# Created:     17/09/2023
+# Copyright:   (c) ediso 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+#Definir dos variables de distinto valor numérico que ejecuten una operación
+
+    x = 500
+    y = 100
+
+    suma = x + y
+
+#y finalmente semuestren por pantalla.
+
+    print (suma)
+
+if __name__ == '__main__':
+    main()
