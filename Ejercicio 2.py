8e2bca311aeb307a93f923b1e6c7ece1599b12ac
edison vilchez <tu@email>
2023-09-17 21:56:40 -0300

    Mi segundo ejercicio subido a git

---
+++ Ejercicio 2.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ediso
+#
+# Created:     17/09/2023
+# Copyright:   (c) ediso 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+
+    #Definir dos variables con diferentes cadenas de texto que se concatenen.
+    nombre = "mi nombre es Edison  Vilchez"
+    domicilio = " vivo en la calle 118"
+
+    nombre + domicilio
+
+    #y finalmente semuestren por pantalla.
+    print (nombre + domicilio)
+
+
+if __name__ == '__main__':
+    main()
