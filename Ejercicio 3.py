ddeedb99011abcb842200d0d0e05fdea886d5da2
edison vilchez <tu@email>
2023-09-18 19:42:08 -0300

    Mi tercer ejercicio subido a git

---
+++ Ejercicio 3.py
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ediso
+#
+# Created:     18/09/2023
+# Copyright:   (c) ediso 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    nombre = input("ingrese su nombre")
+    dni = input ("ingrese su Dni")
+
+    print(f'El nombre del usuario es {nombre} y su dni es {dni}')
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
